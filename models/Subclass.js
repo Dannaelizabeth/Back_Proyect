3f9edf06c52495c2a6d632a976d29dd73a4df3b3
Dannaelizabeth <dannaelizabethrb@gmail.com>
2023-05-22 19:04:59 -0400

    .

---
+++ models/Subclass.js
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database";
+
+const{DataTypes} = Sequelize;
+
+const Subclass = db.define('subclass', {
+
+})
+
+export default Subclass;
